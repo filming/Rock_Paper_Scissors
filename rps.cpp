a9871a1e16bada116e69b867a8eeadffcca91b74
Filming <79931565+filming@users.noreply.github.com>
2023-05-02 07:07:07 -0400

    Add main file

---
+++ rps.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+int main(){
+    std::cout << "Welcome To Rock, Paper, Scissors!\n\n";
+
+    // get user input
+    int user_choice = get_user_choice();
+    std::string user_choice_str = choice_str(user_choice);
+
+    // get computer choice
+    srand(time(NULL));
+
+    int computer_choice = rand() % 3 + 1;
+    std::string computer_choice_str = choice_str(computer_choice);
+
+    // displaying the end result
+    game_status(user_choice_str, computer_choice_str);
+    
+}
